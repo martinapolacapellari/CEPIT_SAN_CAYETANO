598383da04394970a3311eff764715033819618a
martinapolacapellari <martinapolacapellari@gmail.com>
2023-03-21 15:29:44 -0300

    variables suma

--- Slide2/playground.ts
+++ Slide2/playground.ts
@@ -1,5 +1,10 @@
-import * as rls from "readline-sync";
-let nombre:string=" ";
+//escriba un algoritmo que lea dos numeros por teclado e imprima en consola el resultado de la suma...
 
-nombre= rls.question("ingrese su nombre:");
-console.log("usted ingreso",nombre)
+import * as rls from "readline-sync";
+let num1: number = 0;
+let num2: number = 0;
+
+num1 = rls.questionInt("ingrese el primer numero entero: ");
+num2 = rls.questionInt("ingrese el segundo numero entero: ");
+
+console.log("el resultado de la suma es", num1 + num2);
