013355be8019d8dcbd73768eb52fe9801c3b1cea
martinapolacapellari <martinapolacapellari@gmail.com>
2023-03-21 15:17:08 -0300

    tareas granjeras

---
+++ Slide2/tareasgranjera.ts
@@ -0,0 +1,35 @@
+// algoritmos secuenciales
+//programa 1
+console.log("avanzar uno");
+console.log("quitar uno");
+console.log("avanzar uno");
+console.log("quitar uno");
+console.log("avanzar uno");
+console.log("quitar uno");
+console.log("avanzar uno");
+console.log("quitar uno");
+console.log("girar izquierda  90 grados");
+console.log("avanzar uno");
+console.log("quitar uno");
+console.log("avanzar uno");
+console.log("quitar uno");
+console.log("avanzar uno");
+console.log("quitar uno");
+console.log("avanzar uno");
+console.log("quitar uno");
+
+//programa 2
+console.log("avanzar uno");
+console.log("llenar uno");
+console.log("avanzar uno");
+console.log("avanzar uno");
+console.log("avanzar uno");
+console.log("llenar uno");
+console.log("avanzar uno");
+console.log("quitar uno");
+console.log("avanzar uno");
+console.log("quitar uno");
+console.log("avanzar uno");
+console.log("quitar uno");
+console.log("avanzar uno");
+console.log("quitar uno");
