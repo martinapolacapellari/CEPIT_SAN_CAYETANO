037db6591ca1eaa8821e7261b3d8c06a468628ea
martinapolacapellari <martinapolacapellari@gmail.com>
2023-03-21 15:13:57 -0300

    Ejercicio1

---
+++ Ejercicio 1.ts
@@ -0,0 +1,11 @@
+console.log("Prender el horno y dejarlo a fuego minimo ")
+console.log("Enmantecar y enharinar el reciepiente en el que vas a poner la mezcla")
+console.log("Poner en un bowl 3 huevos y 220 ml de leche");
+console.log("Batir energicamente 3-4 minutos con batidora electrica");
+console.log("Batir energicamente 10 minutos a mano");
+console.log("Agregas el contenido el sobre");
+console.log("Mesclar todo el contenido");
+console.log("Verter el contenido en el recipiente enmantecado y enharinado");
+console.log("Colocarlo en el horno y dejarlo durante 60 minutos, vigilandolo cada tanto");
+console.log("Sacarlo pasado el tiempo previsto y dejarlo enfriar");
+console.log("Una vez enfriado desmoldarlo y listo para comer");
